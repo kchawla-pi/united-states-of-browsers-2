87194c47a3b35f1581fda6e0793fd449eb74712f
Kshitij Chawla <kc.insight.pi@gmail.com>
2022-05-11 01:26:22 +0200

    Experiment with ports and adapters

---
+++ united-states-of-browsers-2/ports/configuration.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+from abc import ABCMeta
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+from typing import Collection
+
+from pydantic import BaseModel
+
+class Profile:
+    ...
